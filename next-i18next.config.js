d8cbf2e37a09bb25db8770af9e323f3114cbc1bb
Romulo Carvalho <romulo.carvalho@mblabs.com.br>
2023-01-27 18:04:59 -0300

    refactor: delete next-i18n config

--- next-i18next.config.js
+++
@@ -1,7 +0,0 @@
-// eslint-disable-next-line no-undef
-module.exports = {
-  i18n: {
-    defaultLocale: "pt-BR",
-    locales: ["pt-BR"],
-  },
-};
--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import Login from "./pages/Login/Login";
+import { AdvancedButton } from "./components";
 
 import "antd/dist/reset.css";
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <>
       <Login />
+      <AdvancedButton />
     </>
   );
 }
