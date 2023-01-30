7a06f5a0e0cd0ebfd8bb4f462cc7d3bdb4a22511
Romulo Carvalho <romulo.carvalho@mblabs.com.br>
2023-01-30 14:10:23 -0300

    fix: router in production

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable consistent-return */
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { HashRouter, Navigate, Route, Routes } from "react-router-dom";
 import NotFound from "src/pages/Error/not-found/NotFound";
 import LayoutPage from "src/pages/Layout";
 import Login from "src/pages/Login/LoginForm";
@@ -9,26 +9,9 @@ import routeList from "./routeList";
 
 function Router() {
   const user = { token: true, userInfo: { role: 1 } };
-  const { userInfo } = user;
 
-  const formatRole = (role: number): string | undefined => {
-    switch (role) {
-      case 1:
-        return "user";
-      case 2:
-        return "admin";
-      case 3:
-        return "root";
-      default:
-        break;
-    }
-  };
-
-  const handleFilterComponent = (route: any) => {
-    return route.roles.includes(formatRole(userInfo.role));
-  };
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/login" element={<Login />} />
         {/* <Route path="/forget" element={<Forget />} /> */}
@@ -38,13 +21,11 @@ function Router() {
             <Route index element={<Navigate to="/home" />} />
             {routeList.map(route => {
               return (
-                handleFilterComponent(route) && (
-                  <Route
-                    key={route?.path}
-                    path={route?.path}
-                    element={<route.component />}
-                  />
-                )
+                <Route
+                  key={route?.path}
+                  path={route?.path}
+                  element={<route.component />}
+                />
               );
             })}
           </Route>
@@ -52,7 +33,7 @@ function Router() {
           <Route path="*" element={<Navigate to="/login" replace={true} />} />
         )}
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
